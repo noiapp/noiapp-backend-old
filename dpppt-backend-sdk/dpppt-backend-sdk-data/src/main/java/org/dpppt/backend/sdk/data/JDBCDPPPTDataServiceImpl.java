aa315934982be530e2b11aabd01d2337ac922fb3
Ciro Cardone <c.cardone@widegroup.ch>
2020-04-21 11:14:22 +0200

    #15 add upsert fo hsqldb

--- dpppt-backend-sdk/dpppt-backend-sdk-data/src/main/java/org/dpppt/backend/sdk/data/JDBCDPPPTDataServiceImpl.java
+++ dpppt-backend-sdk/dpppt-backend-sdk-data/src/main/java/org/dpppt/backend/sdk/data/JDBCDPPPTDataServiceImpl.java
@@ -42,6 +42,7 @@ public class JDBCDPPPTDataServiceImpl implements DPPPTDataService {
         } else {
             sql = "merge into t_exposed using (values(cast(:key as varchar(10000)), cast(:onset as date), cast(:app_source as varchar(50))))"
                 + " as vals(key, onset, app_source) on t_exposed.key = vals.key"
+                + " when matched then update set onset = vals.onset, app_source = vals.app_source"
                 + " when not matched then insert (key, onset, app_source) values (vals.key, vals.onset, vals.app_source)";
         }
         MapSqlParameterSource params = new MapSqlParameterSource();
