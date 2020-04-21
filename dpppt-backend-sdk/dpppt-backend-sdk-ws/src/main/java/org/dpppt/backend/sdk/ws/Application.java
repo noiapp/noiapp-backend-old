94ed8b063bad060954e26ba49f740914d93df5f3
Ciro Cardone <c.cardone@widegroup.ch>
2020-04-21 16:40:11 +0200

    #23 add discovery endpoint

--- dpppt-backend-sdk/dpppt-backend-sdk-ws/src/main/java/org/dpppt/backend/sdk/ws/Application.java
+++ dpppt-backend-sdk/dpppt-backend-sdk-ws/src/main/java/org/dpppt/backend/sdk/ws/Application.java
@@ -7,15 +7,9 @@
 package org.dpppt.backend.sdk.ws;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@ComponentScan(basePackages = {"org.dpppt.backend.sdk.ws.config"})
-@EnableAutoConfiguration
-@EnableWebMvc
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
