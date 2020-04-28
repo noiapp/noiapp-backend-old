d77978ed74ed7dbc8c9312e5b42d07984756d050
Ciro Cardone <c.cardone@widegroup.ch>
2020-04-28 12:46:48 +0200

    add check on dump failed, improve remove old dumps

--- bin/dump-postgres.sh
+++ bin/dump-postgres.sh
@@ -9,10 +9,16 @@ mkdir -p "$DUMPDIR"
 
 # exec dump
 docker exec noiapp_noiapp-database_1 bash -c 'pg_dump --create --format t -U noiapp'>"$DESTFILE"
+RET=$?
+if [ $RET -ne 0 ] ; then
+  rm "$DESTFILE"
+  echo "DB dump has failed, $DESTFILE not created." >&2
+  exit $RET
+fi
 
 # create latest link
 rm $LATEST
 ln -s "$DESTFILE" "$LATEST"
 
 # retain only last 2 dumps and latest link
-ls -t "$DUMPDIR" | tail -n +4 | xargs rm --
+find $DUMPDIR -type f -printf '%T@\t%p\n' | sort -t $'\t' -g | head -n -2 | cut -d $'\t' -f 2- | xargs rm
