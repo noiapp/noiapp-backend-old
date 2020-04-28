08ee1c5dba87ec2a3acfe2f8b7b2b5156d8dff45
Ciro Cardone <c.cardone@widegroup.ch>
2020-04-28 10:35:14 +0200

    fix after brushtyler review

--- bin/dump-postgres.sh
+++ bin/dump-postgres.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 DATE=$(date +"%Y-%m-%d-%H-%M")
-
 DUMPDIR=/home/noiapp/dump
 DESTFILE=$DUMPDIR/dump-$DATE.tar
 LATEST=$DUMPDIR/latest
-ls -t | tail -n +5 | xargs rm --
-docker exec noiapp_noiapp-database_1 bash -c 'pg_dump --create --format t -U noiapp'>$DESTFILE
+
+mkdir -p "$DUMPDIR"
+
+# exec dump
+docker exec noiapp_noiapp-database_1 bash -c 'pg_dump --create --format t -U noiapp'>"$DESTFILE"
+
+# create latest link
 rm $LATEST
-ln -s $DESTFILE $LATEST
-ls -t $DUMPDIR | tail -n +4 | xargs rm --
+ln -s "$DESTFILE" "$LATEST"
+
+# retain only last 2 dumps and latest link
+ls -t "$DUMPDIR" | tail -n +4 | xargs rm --
